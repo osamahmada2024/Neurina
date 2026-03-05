90425a2f9ff6b0e3865dd04234979c73b0d57a7d
osamahmada2024 <hmsosamagithub773@gmail.com>
2026-03-05 16:17:56 +0200

    Implement FastAPI application structure with initial root endpoint

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-c
+from fastapi import FastAPI
+from .config import settings
+
+app = FastAPI(
+    title = settings.APP_NAME,
+    version = settings.APP_VERSION,
+    debug = settings.DEBUG
+)
+
+@app.get('/')
+async def root():
+
+    return {
+        "App Name": settings.APP_NAME,
+        "App Version": settings.APP_VERSION,
+        "message" : "Welcome to the FastAPI application!"
+    }
+    
