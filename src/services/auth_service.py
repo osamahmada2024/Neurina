9e53d0d9dcd8e3b94e8d92ed536a7b4e1fd10faa
osamahmada2024 <hmsosamagithub773@gmail.com>
2026-03-08 02:05:52 +0200

    Enhance password validation: update error messages for minimum length, maximum length, uppercase, lowercase, digit, and special character requirements.

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -25,14 +25,15 @@ def verify_access_token(token: str):
 def verify_strong_password(password: str) -> str:
 
     if len(password) < 8:
-        return Password_Exceeded.BELOW_MIN_LENGTH
+        return Password_Exceeded.EXCEED_MIN_LENGTH
+    if len(password) > 128:
+        return Password_Exceeded.EXCEED_MAX_LENGTH
     if not any(char.isupper() for char in password):
-        return Password_Exceeded.MISSING_UPPERCASE
+        return Password_Exceeded.EXCEED_REQUIRE_UPPERCASE
     if not any(char.islower() for char in password):
-        return Password_Exceeded.MISSING_LOWERCASE
+        return Password_Exceeded.EXCEED_REQUIRE_LOWERCASE
     if not any(char.isdigit() for char in password):
-        return Password_Exceeded.MISSING_DIGIT
+        return Password_Exceeded.EXCEED_REQUIRE_DIGIT
     if not any(char in "!@#$%^&*()-_=+[]{}|;:'\",.<>?/" for char in password):
-        return Password_Exceeded.MISSING_SPECIAL_CHARACTER
+        return Password_Exceeded.EXCEED_REQUIRE_SPECIAL_CHAR
     return Password_Exceeded.VALID
-    
